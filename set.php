ff35db141a20dab0d98dda9adc7c49a0b9033502
Hanani Rafei <hanani@zen.com.my>
2021-01-13 18:24:25 +0800

    update webhook url

--- set.php
+++ set.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1454870068:AAHOlq51Sb4XdXOprApaGXH9wB0Q1CiB8JE';
 $bot_username = 'hananifirstbot_bot';
-$hook_url     = 'https://api.telegram.org/bot1454870068:AAHOlq51Sb4XdXOprApaGXH9wB0Q1CiB8JE/setwebhook?url=https://127.0.0.1:80/hook.php';
+$hook_url     = 'https://api.telegram.org/bot1454870068:AAHOlq51Sb4XdXOprApaGXH9wB0Q1CiB8JE/setwebhook?url=https://hanani.happyforever.com/hook.php';
 
 try {
     // Create Telegram API object
