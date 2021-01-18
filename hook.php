735af00f448b492bf5a8f6819d9606f7648c9fa8
Hanani Rafei <hanani@zen.com.my>
2021-01-18 17:08:23 +0800

    update hook php file

--- hook.php
+++ hook.php
@@ -1,6 +1,6 @@
 <?php
 // Load composer
-require __DIR__ . './vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1454870068:AAHOlq51Sb4XdXOprApaGXH9wB0Q1CiB8JE';
 $bot_username = 'hananifirstbot_bot';
