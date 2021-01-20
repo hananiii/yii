3da7dbaf23bb526d800147473310e6405390653e
Hanani Rafei <hanani@zen.com.my>
2021-01-21 07:29:55 +0800

    apply chart js

--- views/site/about.php
+++ views/site/about.php
@@ -2,10 +2,15 @@
 
 /* @var $this yii\web\View */
 
+
 use yii\helpers\Html;
+use yii\helpers\Url;
+use \dosamigos\chartjs\ChartJs;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+
+
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -18,4 +23,50 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h2>Generate PDF</h2>
     <button onclick="document.location='index.php?r=site%2Fgeneratepdf'">Click</button>
+
+    <h2>Save as</h2>
+    <li>
+        <button>PNG</button>
+        <button>SVG</button>
+        <button>JPEG</button>
+
+    </li>
+
+    <h2>Charts</h2>
+    <div>
+        jjj
+    </div>
 </div>
+<?= ChartJs::widget([
+    'type' => 'line',
+    'options' => [
+        'height' => 100,
+        'width' => 300
+    ],
+    'data' => [
+        'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+        'datasets' => [
+            [
+                'label' => "My First dataset",
+                'backgroundColor' => "rgba(179,181,198,0.2)",
+                'borderColor' => "rgba(179,181,198,1)",
+                'pointBackgroundColor' => "rgba(179,181,198,1)",
+                'pointBorderColor' => "#fff",
+                'pointHoverBackgroundColor' => "#fff",
+                'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                'data' => [65, 59, 90, 81, 56, 55, 40]
+            ],
+            [
+                'label' => "My Second dataset",
+                'backgroundColor' => "rgba(255,99,132,0.2)",
+                'borderColor' => "rgba(255,99,132,1)",
+                'pointBackgroundColor' => "rgba(255,99,132,1)",
+                'pointBorderColor' => "#fff",
+                'pointHoverBackgroundColor' => "#fff",
+                'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                'data' => [28, 48, 40, 19, 96, 27, 100]
+            ]
+        ]
+    ]
+]);
+?>
